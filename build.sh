4e683ea9a162539cebd178f09887dc7182793dea
Stilic <lateman-jpeg@outlook.fr>
2024-07-06 12:04:31 +0200

    additionnal stuff

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd initramfs
+cp -r ../base/* .
 find . -print0 | cpio --null -o --format=newc | gzip -9 > ../initramfs.cpio.gz
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-qemu-system-x86_64 -accel kvm -kernel sources/linux-6.6.35/arch/x86_64/boot/bzImage -initrd initramfs.cpio.gz -nographic -append "quiet console=ttyS0"
+exec qemu-system-x86_64 -accel kvm -kernel sources/linux-6.6.35/arch/x86_64/boot/bzImage -initrd initramfs.cpio.gz -nographic -append "quiet console=ttyS0"
