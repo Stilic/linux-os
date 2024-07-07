2f70b7da0f5fb34d7b35b62656dab84026bbb425
Stilic <lateman-jpeg@outlook.fr>
2024-07-07 13:41:33 +0200

    upgrade the stuff

--- setup.sh
+++ setup.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+function extract_source {
+     local loc="$1"
+     local name=$(basename $loc)
+     curl -o $name $loc
+     loc=${name%-*}
+     mkdir $loc
+     tar -xf $name --strip-components=1 -C $loc
+     rm $name
+}
 mkdir sources initramfs
 cd sources
 # TODO: get versions using args
-curl -o linux.tar.xz https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.37.tar.xz \
-     -o busybox.tar.bz2 https://busybox.net/downloads/busybox-1.36.1.tar.bz2
+extract_source https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.37.tar.xz
+extract_source https://busybox.net/downloads/busybox-1.36.1.tar.bz2
