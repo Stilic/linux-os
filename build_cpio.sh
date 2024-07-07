63e4de91971c2e30ed1cc8bedbbb8e6389ea388a
Stilic <lateman-jpeg@outlook.fr>
2024-07-07 17:38:40 +0200

    uhh

--- build_cpio.sh
+++ build_cpio.sh
@@ -3,6 +3,6 @@ rm -rf initramfs
 mkdir initramfs
 cd initramfs
 mkdir dev proc sys etc sbin bin usr
-cp -a ../sources/busybox/_install/{bin,sbin,usr} .
+cp -a ../sources/busybox/_install/* .
 cp -r ../base/* .
 find . -print | cpio -o --format=newc | gzip -9 > ../initramfs.cpio.gz
