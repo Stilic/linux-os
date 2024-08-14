5ae7f4ce86ccfb00c15e9e8c93e4e7a3b69fe9a5
Stilic <lateman-jpeg@outlook.fr>
2024-08-14 21:21:37 +0000

    guh

--- ramdisk_init.sh
+++ ramdisk_init.sh
@@ -4,5 +4,5 @@ mount -t proc proc /proc
 mount -t sysfs sysfs /sys
 mount $(sed -e 's/^.*root=//' -e 's/ .*$//' /proc/cmdline) /mnt
 umount /dev /proc /sys
-export LD_LIBRARY_PATH=/mnt/lib:/mnt/usr/lib
+export LD_LIBRARY_PATH=/lib:/usr/lib
 exec switch_root /mnt /init
