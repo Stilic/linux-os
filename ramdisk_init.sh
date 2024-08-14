bf2a58407bad6283f9f1758dd49f770c35d25bdf
Stilic <lateman-jpeg@outlook.fr>
2024-08-14 12:24:47 +0200

    Update ramdisk_init.sh

--- ramdisk_init.sh
+++ ramdisk_init.sh
@@ -4,4 +4,5 @@ mount -t proc proc /proc
 mount -t sysfs sysfs /sys
 mount $(sed -e 's/^.*root=//' -e 's/ .*$//' /proc/cmdline) /mnt
 umount /dev /proc /sys
+export LD_LIBRARY_PATH=/mnt/lib:/mnt/usr/lib
 exec switch_root /mnt /init
