eca6292d62b7bdfef6370fa2918d51fa0b275309
Stilic <lateman-jpeg@outlook.fr>
2024-10-25 23:31:31 +0200

    bump versions

--- setup.sh
+++ setup.sh
@@ -12,6 +12,6 @@ truncate -s 1GB disk
 mkdir mnt sources
 cd sources
 # TODO: get versions using args
-extract_source https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.37.tar.xz
-extract_source https://ftp.gnu.org/gnu/glibc/glibc-2.39.tar.xz
+extract_source https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.6.58.tar.xz
+extract_source https://ftp.gnu.org/gnu/glibc/glibc-2.40.tar.xz
 extract_source https://busybox.net/downloads/busybox-1.36.1.tar.bz2
